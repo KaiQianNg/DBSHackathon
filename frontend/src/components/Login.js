22b0074848606cad385f55b4cad490ef7daaa5d5
damianchenhk <69572321+damianchenhk@users.noreply.github.com>
2022-01-20 12:17:06 +0800

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {Col, Row, FloatingLabel, Form, Button, Container} from "react-bootstrap"
+import {Col, Row, Form, Button, Container} from "react-bootstrap"
 
 const Login = () => {
 
@@ -8,45 +8,23 @@ const Login = () => {
         <>
             <Container>
                 <Row>
-                    <h1><b>Add Courses Here</b></h1>
+                    <h1><b>Login</b></h1>
                     <Form>
-                        <FloatingLabel
-                                    controlId="floatingInput"
-                                    label="Email"
-                                    className="mb-3"
-                                    style={{
-                                        float:'left',
-                                        marginRight:'10px'
-                                    }}>
-                            <Form.Control
-                                        type="email"
-                                        name="email"
-                                        placeholder="Email"
-                                        style={{
-                                            marginLeft: '10px',
-                                            float: 'right'
-                                        }}
-                                    />
-                        </FloatingLabel>
-                        <FloatingLabel
-                                    controlId="floatingInput"
-                                    label="Password"
-                                    className="mb-3"
-                                    style={{
-                                        float:'left'
-                                    }}>
-                            <Form.Control
-                                        type="password"
-                                        name="password"
-                                        placeholder="Password"
-                                        style={{
-                                            marginLeft: '10px',
-                                            float: 'right'
-                                        }}
-                                    />
-                        </FloatingLabel>
-                        <br></br>
-                        <Button variant="primary" type="submit" style={{marginBottom: '30px'}}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
                             Submit
                         </Button>
                     </Form>

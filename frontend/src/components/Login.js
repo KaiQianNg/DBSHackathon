5f41c721906a9fc0621a5eb54c080422d7961510
damianchenhk <69572321+damianchenhk@users.noreply.github.com>
2022-01-20 12:34:02 +0800

    added login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Row, Form, Button, Container} from "react-bootstrap"
+import { Row, Form, Button, Container} from "react-bootstrap";
+import axios from "axios";
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const onSubmit = () => {
+
+    }
 
     return(
         <>
@@ -12,14 +20,22 @@ const Login = () => {
                     <Form>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                            </Form.Text>
+                            <Form.Control 
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}    
+                            />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control
+                                type="password"
+                                placeholder="Password" 
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
                         </Form.Group>
                         <Button variant="primary" type="submit">
                             Submit
